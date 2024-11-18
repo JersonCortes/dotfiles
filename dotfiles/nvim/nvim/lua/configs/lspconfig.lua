85369c63c5823c0bc84e9c6e0b35aad71d141786
Jerson Cortes <JersonCortes@proton.me>
2024-11-18 15:51:27 -0600

    updated nvim for test

---
+++ dotfiles/nvim/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,25 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+ "asm_lsp","clangd","cmake","cssls","dockerls","pyright","rust_analyzer"
+}
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.html.setup{
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+    filetypes = { "html", "rust" }
+}

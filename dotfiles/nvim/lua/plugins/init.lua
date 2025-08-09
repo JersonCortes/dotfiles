89515849fb421af867fa312d0e98e16231600f08
JersonCortes <JersonCortes@proton.me>
2025-08-08 22:55:41 -0600

    Added plugin as dependency for devcontainers

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -30,6 +30,7 @@ return {
     'jedrzejboczar/devcontainers.nvim',
     dependencies = {
       'miversen33/netman.nvim', -- optional to browse files in docker container
+      'stevearc/overseer.nvim', -- see progress and notificacions
     },
   },
 
